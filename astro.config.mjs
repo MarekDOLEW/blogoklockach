1ff0afc573a6317b3f505acce7c9a91631bb2713
MarekDOLEW <marek.dolewski@gmail.com>
2026-08-12 08:50:30 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://blogoklockach.pl',
+  site: 'https://tylkoklocki.pl',
   integrations: [sitemap()],
   build: { inlineStylesheets: 'auto' }
 });
